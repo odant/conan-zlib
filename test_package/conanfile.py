f545baa6b609a434fd489ac6efe59ab5809216f1
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2017-12-19 02:47:10 +0300

    RPATH  linking

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -18,7 +18,7 @@ class ZlibTestConan(ConanFile):
     def imports(self):
         self.copy("*.pdb", dst="bin", src="bin")
         self.copy("*.dll", dst="bin", src="bin")
-        self.copy("*.dylib*", dst="bin", src="lib")
+        self.copy("*.so.*", dst="bin", src="lib")
 
     def test(self):
         if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio":
