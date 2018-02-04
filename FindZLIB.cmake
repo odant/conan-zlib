1d820a95d19f22656cf36e01123453e64affcf8a
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2018-02-04 23:17:09 +0300

    Custom FindZLIB.cmake

--- FindZLIB.cmake
+++ FindZLIB.cmake
@@ -1,24 +1,53 @@
-message(STATUS "Conan: Advance FindZLIB.cmake, adjusting variable ZLIB_LIBRARY")
+find_path(ZLIB_INCLUDE_DIR
+    NAMES zlib.h
+    PATHS ${CONAN_INCLUDE_DIRS_ZLIB}
+    NO_DEFAULT_PATH
+)
 
-list(GET CONAN_LIBS_ZLIB 0 _zlib_name)
 find_library(ZLIB_LIBRARY
-    NAMES ${_zlib_name}
+    NAMES z zlib zlibd zlibstatic zlibstaticd
     PATHS ${CONAN_LIB_DIRS_ZLIB}
     NO_DEFAULT_PATH
 )
 
-include(${CMAKE_ROOT}/Modules/FindZLIB.cmake)
+if(ZLIB_INCLUDE_DIR AND EXISTS ${ZLIB_INCLUDE_DIR}/zlib.h)
+    file(STRINGS ${ZLIB_INCLUDE_DIR}/zlib.h ZLIB_H REGEX "^#define ZLIB_VERSION \"[^\"]*\"$")
+
+    string(REGEX REPLACE "^.*ZLIB_VERSION \"([0-9]+).*$" "\\1" ZLIB_VERSION_MAJOR "${ZLIB_H}")
+    string(REGEX REPLACE "^.*ZLIB_VERSION \"[0-9]+\\.([0-9]+).*$" "\\1" ZLIB_VERSION_MINOR  "${ZLIB_H}")
+    string(REGEX REPLACE "^.*ZLIB_VERSION \"[0-9]+\\.[0-9]+\\.([0-9]+).*$" "\\1" ZLIB_VERSION_PATCH "${ZLIB_H}")
+    set(ZLIB_VERSION_STRING "${ZLIB_VERSION_MAJOR}.${ZLIB_VERSION_MINOR}.${ZLIB_VERSION_PATCH}")
+
+    # only append a TWEAK version if it exists:
+    set(ZLIB_VERSION_TWEAK "")
+    if( "${ZLIB_H}" MATCHES "ZLIB_VERSION \"[0-9]+\\.[0-9]+\\.[0-9]+\\.([0-9]+)")
+        set(ZLIB_VERSION_TWEAK "${CMAKE_MATCH_1}")
+        string(APPEND ZLIB_VERSION_STRING ".${ZLIB_VERSION_TWEAK}")
+    endif()
+
+    set(ZLIB_MAJOR_VERSION "${ZLIB_VERSION_MAJOR}")
+    set(ZLIB_MINOR_VERSION "${ZLIB_VERSION_MINOR}")
+    set(ZLIB_PATCH_VERSION "${ZLIB_VERSION_PATCH}")
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(ZLIB
+    REQUIRED_VARS ZLIB_LIBRARY ZLIB_INCLUDE_DIR
+    VERSION_VAR ZLIB_VERSION_STRING
+)
 
 if(ZLIB_FOUND)
-    message(STATUS "Conan: ZLIB_INCLUDE_DIRS => ${ZLIB_INCLUDE_DIRS}")
-    message(STATUS "Conan: ZLIB_LIBRARIES => ${ZLIB_LIBRARIES}")
-    get_target_property(INTERFACE_INCLUDE_DIRECTORIES ZLIB::ZLIB INTERFACE_INCLUDE_DIRECTORIES)
-    message(STATUS "Conan: ZLIB::ZLIB INTERFACE_INCLUDE_DIRECTORIES => ${INTERFACE_INCLUDE_DIRECTORIES}")
-    get_target_property(IMPORTED_LOCATION ZLIB::ZLIB IMPORTED_LOCATION)
-    message(STATUS "Conan: ZLIB::ZLIB IMPORTED_LOCATION => ${IMPORTED_LOCATION}")
-    
-    unset(INTERFACE_INCLUDE_DIRECTORIES)
-    unset(IMPORTED_LOCATION)
+    set(ZLIB_INCLUDE_DIRS ${ZLIB_INCLUDE_DIR})
+    set(ZLIB_LIBRARIES ${ZLIB_LIBRARY})
+    mark_as_advanced(ZLIB_INCLUDE_DIR ZLIB_LIBRARY)
+
+    if(NOT TARGET ZLIB::ZLIB)
+      add_library(ZLIB::ZLIB UNKNOWN IMPORTED)
+      set_target_properties(ZLIB::ZLIB PROPERTIES
+        INTERFACE_INCLUDE_DIRECTORIES ${ZLIB_INCLUDE_DIRS}
+        IMPORTED_LOCATION ${ZLIB_LIBRARY}
+      )
+    endif()
 
     set(_enable_minizip)
     foreach(_item ${${CMAKE_FIND_PACKAGE_NAME}_FIND_COMPONENTS})
@@ -34,14 +63,8 @@ if(ZLIB_FOUND)
             NO_DEFAULT_PATH
         )
     
-        foreach(_item ${CONAN_LIBS_ZLIB})
-            set(_minizip_name)
-            if(${_item} MATCHES "minizip")
-                set(_minizip_name ${_item})
-            endif()
-        endforeach()
         find_library(MINIZIP_LIBRARY
-            NAMES ${_minizip_name}
+            NAMES minizip minizipd minizipstatic minizipstaticd
             PATHS ${CONAN_LIB_DIRS_ZLIB}
             NO_DEFAULT_PATH
         )
@@ -52,7 +75,6 @@ if(ZLIB_FOUND)
         set(MINIZIP_VERSION_STRING "${MINIZIP_VERSION_MAJOR}.${MINIZIP_VERSION_MINOR}.${MINIZIP_VERSION_PATCH}")
         
         set(MINIZIP_FIND_REQUIRED ON)
-        include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
         find_package_handle_standard_args(MINIZIP
             REQUIRED_VARS MINIZIP_LIBRARY MINIZIP_INCLUDE_DIR
             VERSION_VAR MINIZIP_VERSION_STRING
@@ -61,33 +83,17 @@ if(ZLIB_FOUND)
         if(MINIZIP_FOUND)
             set(MINIZIP_INCLUDE_DIRS ${MINIZIP_INCLUDE_DIR})
             set(MINIZIP_LIBRARIES ${MINIZIP_LIBRARY})
+            mark_as_advanced(MINIZIP_INCLUDE_DIR MINIZIP_LIBRARY)
 
             if(NOT TARGET ZLIB::minizip)
                 add_library(ZLIB::minizip UNKNOWN IMPORTED)
                 set_target_properties(ZLIB::minizip PROPERTIES
-                    INTERFACE_INCLUDE_DIRECTORIES "${MINIZIP_INCLUDE_DIRS}"
-                    IMPORTED_LOCATION "${MINIZIP_LIBRARIES}"
-                    INTERFACE_LINK_LIBRARIES "ZLIB::ZLIB"
+                    INTERFACE_INCLUDE_DIRECTORIES ${MINIZIP_INCLUDE_DIR}
+                    IMPORTED_LOCATION ${MINIZIP_LIBRARY}
+                    INTERFACE_LINK_LIBRARIES ZLIB::ZLIB
                 )
             endif()
+        endif() # if(MINIZIP_FOUND)
+    endif() # if(_enable_minizip)
+endif() # if(ZLIB_FOUND)
 
-            mark_as_advanced(MINIZIP_INCLUDE_DIR MINIZIP_LIBRARY)
-
-            message(STATUS "Conan: MINIZIP_INCLUDE_DIRS => ${MINIZIP_INCLUDE_DIRS}")
-            message(STATUS "Conan: MINIZIP_LIBRARIES => ${MINIZIP_LIBRARIES}")
-            get_target_property(INTERFACE_INCLUDE_DIRECTORIES ZLIB::minizip INTERFACE_INCLUDE_DIRECTORIES)
-            message(STATUS "Conan: ZLIB::minizip INTERFACE_INCLUDE_DIRECTORIES => ${INTERFACE_INCLUDE_DIRECTORIES}")
-            get_target_property(IMPORTED_LOCATION ZLIB::minizip IMPORTED_LOCATION)
-            message(STATUS "Conan: ZLIB::minizip IMPORTED_LOCATION => ${IMPORTED_LOCATION}")
-            
-            unset(INTERFACE_INCLUDE_DIRECTORIES)
-            unset(IMPORTED_LOCATION)
-        endif()
-
-    endif()
-
-endif()
-
-unset(_zlib_name)
-unset(_enable_minizip)
-unset(_item)
