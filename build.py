67a57507c953a0eac7d1f1d0da9aeeceba3598b0
Dmitriy Vetutnev <d.vetutnev@gmail.com>
2019-06-05 15:14:14 +0300

    Add MT/MTd build for MSVC. Remove build without minizip.

--- build.py
+++ build.py
@@ -1,5 +1,4 @@
 import platform, os
-from copy import deepcopy
 from conan.packager import ConanMultiPackager
 
 
@@ -7,49 +6,25 @@ from conan.packager import ConanMultiPackager
 username = "odant" if "CONAN_USERNAME" not in os.environ else None
 # Windows settings
 visual_versions = ["14", "15"] if "CONAN_VISUAL_VERSIONS" not in os.environ else None
-visual_runtimes = ["MD", "MDd"] if "CONAN_VISUAL_RUNTIMES" not in os.environ else None
-visual_default_toolsets = {
-    "14": [None, "v140_xp"],
-    "15": [None, "v141_xp"]
-}
-visual_toolsets = None
-if "CONAN_VISUAL_TOOLSETS" in os.environ:
-    visual_toolsets = [s.strip() for s in os.environ["CONAN_VISUAL_TOOLSETS"].split(",")]
+visual_runtimes = ["MD", "MDd", "MT", "MTd"] if "CONAN_VISUAL_RUNTIMES" not in os.environ else None
 dll_sign = False if "CONAN_DISABLE_DLL_SIGN" in os.environ else True
 
-def vs_get_toolsets(compiler_version):
-    return visual_toolsets if not visual_toolsets is None else visual_default_toolsets.get(compiler_version)
-    
-def vs_add_toolset(builds):
-    result = []
-    for settings, options, env_vars, build_requires, reference in builds:
-        toolsets = vs_get_toolsets(settings["compiler.version"])
-        if toolsets is None or settings["compiler"] != "Visual Studio":
-            result.append([settings, options, env_vars, build_requires, reference])
-        else:
-            for t in toolsets:
-                settings = deepcopy(settings)
-                settings["compiler.toolset"] = t
-                result.append([settings, options, env_vars, build_requires, reference])
-    return result
 
 def add_dll_sign(builds):
     result = []
     for settings, options, env_vars, build_requires, reference in builds:
-        options = deepcopy(options)
         options["zlib:dll_sign"] = dll_sign
         result.append([settings, options, env_vars, build_requires, reference])
     return result
-    
+
 def add_minizip(builds):
     result = []
     for settings, options, env_vars, build_requires, reference in builds:
-        for minizip in (True, False):
-            options = deepcopy(options)
-            options["zlib:minizip"] = minizip
-            result.append([settings, options, env_vars, build_requires, reference])
+        options["zlib:minizip"] = True
+        result.append([settings, options, env_vars, build_requires, reference])
     return result
-    
+
+
 if __name__ == "__main__":
     builder = ConanMultiPackager(
         username=username,
@@ -61,7 +36,6 @@ if __name__ == "__main__":
     # Adjusting build configurations
     builds = builder.items
     if platform.system() == "Windows":
-        builds = vs_add_toolset(builds)
         builds = add_dll_sign(builds)
     builds = add_minizip(builds)
     # Replace build configurations
