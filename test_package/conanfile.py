afe876f4d27c26b07be936754e6b1f1a745e02d1
d0nc1h0t <udincev@yandex.ru>
2021-03-12 17:28:22 +0300

    Add build support for test_package by ClangCL VS toolchain

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,13 +1,20 @@
 from conans import ConanFile, CMake
 import os
 
-
 class PackageTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
+    def isClangClToolset(self):
+        return True if self.settings.os == "Windows" and self.settings.compiler == "Visual Studio" and str(self.settings.compiler.toolset).lower() == "clangcl" else False
+
+    def build_requirements(self):
+        if not self.isClangClToolset():
+            self.build_requires("ninja/[>=1.10.2]")
+
     def build(self):
-        cmake = CMake(self)
+        cmakeGenerator = "Ninja" if not self.isClangClToolset() else None
+        cmake = CMake(self, generator=cmakeGenerator)
         cmake.definitions["ENABLE_MINIZIP:BOOL"] = self.options["zlib"].minizip
         cmake.configure()
         cmake.build()
